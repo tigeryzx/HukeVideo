7571e5a6376c47d8b3ec074f65afe9376d614eaa
yzx <nekomail@qq.com>
2018-09-13 07:17:54 +0800

    添加项目文件。

---
+++ HukeVideo/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace HukeVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(12, 4);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(411, 21);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(12, 58);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(411, 135);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.DoubleClick += new System.EventHandler(this.txtResult_DoubleClick);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRun.Location = new System.Drawing.Point(348, 199);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "获取";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMsg.Location = new System.Drawing.Point(12, 31);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ReadOnly = true;
+            this.txtMsg.Size = new System.Drawing.Size(411, 21);
+            this.txtMsg.TabIndex = 1;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "HukeVideo";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 234);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtUrl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HukeVideo";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ HukeVideo/Form1.cs
@@ -0,0 +1,234 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HukeVideo
+{
+    public partial class Form1 : Form
+    {
+        static IntPtr nextClipboardViewer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            nextClipboardViewer = (IntPtr)SetClipboardViewer(this.Handle);
+
+            this.txtUrl.Text = "https://huke88.com/course/8325.html?pageType=1&key=C4D";
+        }
+
+        [DllImport("User32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Unicode)]
+        private static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        // defined in winuser.h
+        const int WM_DRAWCLIPBOARD = 0x308;
+        const int WM_CHANGECBCHAIN = 0x030D;
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    TriggerRun();
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam == nextClipboardViewer)
+                        nextClipboardViewer = m.LParam;
+                    else
+                        SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            this.txtResult.Clear();
+            Thread thread = new Thread(x => {
+
+                try
+                {
+                    GetInfo();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("获取失败！:" + ex.Message);
+                }
+                finally
+                {
+                    this.Invoke(new SwitchBtn(SwitchButtonState));
+                }
+
+            });
+
+            thread.Start();
+        }
+
+        private delegate void SetInputValue(TextBox textbox, string value);
+        private delegate void SwitchBtn();
+        private delegate void ShowTipInfo(string msg);
+
+        void GetInfo()
+        {
+            var url = this.txtUrl.Text;
+
+            if (string.IsNullOrEmpty(url))
+            {
+                MessageBox.Show("请先输入视频原网址！");
+                return;
+            }
+
+            this.Invoke(new SwitchBtn(SwitchButtonState));
+
+            var client = new RestClient("https://design.sjcjn.com/robot/index/index");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Postman-Token", "0ddc4d2c-9df2-499e-a623-ac954b4b2475");
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("Referer", url);
+            request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
+            request.AddParameter(
+                "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+                "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"version\"\r\n\r\n1.0.3\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"token\"\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"website\"\r\n\r\n" + url + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\nvideo\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"sid\"\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
+                ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show(string.Format("请求失败 {0}", response.StatusCode));
+                return;
+            }
+
+            var content = response.Content;
+
+            if (string.IsNullOrEmpty(content))
+            {
+                MessageBox.Show("获取视频信息错误!");
+                return;
+            }
+
+            JObject jo = (JObject)JsonConvert.DeserializeObject(content);
+
+            var msg = jo["msg"].Value<string>();
+
+            this.Invoke(new SetInputValue(ShowResult), this.txtMsg, msg);
+
+            if (!jo["data"].HasValues)
+            {
+                MessageBox.Show(string.Format("解析信息出错！:\r\n {0}", content));
+                return;
+            }
+
+            var resultUrl = jo["data"]["url"].Value<string>();
+
+            var js = @"
+                $('#huke88-video').hkPlayer({
+                    'playerVideoUrl': url,
+                    'playerVideoUrlTwo': url,
+                    'error': function (err) {
+                        sendVideoPlayError(playerTypeForSend, (new Date()).valueOf());
+                    },
+                    'play': function () {
+                        $('#huke88-video-play').remove();
+            
+                        if( $('#huke88-video .video-title').size()>0 ){
+                            $('#huke88-video .video-title').remove();
+                        }
+
+                        $('#loginModal').hide();
+                        layer.closeAll();
+                    }
+                });
+            ".Replace("\r\n", "").Replace(" ", "").Replace("\t", "");
+
+            var fristJs = "var url = '" + resultUrl + "';";
+
+            js = fristJs + js;
+
+            this.Invoke(new SetInputValue(ShowResult), this.txtResult, js);
+            var tipMsg = string.Format("处理 {0} 完成，现在你可以粘贴了。", url);
+            this.Invoke(new ShowTipInfo(ShowNotifyTipInfo), tipMsg);
+        }
+
+        void ShowResult(TextBox textbox,string value)
+        {
+            textbox.Text = value;
+            if (textbox.Name == "txtResult")
+                Clipboard.SetText(value);
+        }
+
+        void SwitchButtonState()
+        {
+            if (this.btnRun.Enabled)
+            {
+                this.btnRun.Enabled = false;
+                this.btnRun.Text = "处理中请稍候";
+            }
+            else
+            {
+                this.btnRun.Enabled = true;
+                this.btnRun.Text = "获取";
+            }
+            
+        }
+
+        void SetClip()
+        {
+            this.txtResult.SelectAll();
+            var selText = this.txtResult.Text;
+            if (!string.IsNullOrEmpty(selText))
+                Clipboard.SetText(selText);
+        }
+
+        void TriggerRun()
+        {
+            var text = Clipboard.GetText();
+            if (text.ToLower().StartsWith("https://huke88.com"))
+            {
+                this.txtUrl.Text = Clipboard.GetText();
+                this.btnRun.PerformClick();
+            }
+        }
+
+        void ShowNotifyTipInfo(string msg)
+        {
+            this.notifyIcon1.ShowBalloonTip(1000, "提示", msg, ToolTipIcon.Info);
+        }
+
+        private void txtResult_DoubleClick(object sender, EventArgs e)
+        {
+            SetClip();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            TriggerRun();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ChangeClipboardChain(this.Handle, nextClipboardViewer);
+        }
+    }
+}
